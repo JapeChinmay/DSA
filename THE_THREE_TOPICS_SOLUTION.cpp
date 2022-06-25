26514b1a99016da2b9f538be5869d78767cb3f14
Chinmay Jape <107535441+JapeChinmay@users.noreply.github.com>
2022-06-25 23:30:53 +0530

    Add files via upload

---
+++ THE_THREE_TOPICS_SOLUTION.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ // your code goes here
+
+     int a,b,c,x;
+     cin>>a>>b>>c>>x;
+     if(x==a || x==b || x==c){
+         cout<<"YES"<<endl;
+     }
+     else{
+         cout<<"NO"<<endl;
+     }
+     
+
+ return 0;
+}
