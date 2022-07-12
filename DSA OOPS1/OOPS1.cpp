9eb2acf50d0fbf76b34af8f11c2a653c41fbea51
Chinmay Jape <107535441+JapeChinmay@users.noreply.github.com>
2022-07-12 13:22:10 +0530

    Add files via upload

--- DSA OOPS1/OOPS1.cpp
+++ DSA OOPS1/OOPS1.cpp
@@ -10,6 +10,35 @@ public :
  int age;
 
 
+
+ //Default constructor, now inbuilt constructor is not called.
+ Student() {
+     cout << "called" << endl;
+
+
+ }
+ // our own roll no Constructor, including arg.
+ Student(int r) {
+
+     cout << " called for roll no " << endl;
+
+     rollNumber = r;
+
+
+ }
+
+
+ // constructor for both roll no and age.
+ Student(int age, int rollNumber)
+ {
+     cout << "called for both"<<endl;
+
+     this ->age = age;
+     this ->rollNumber  = rollNumber;
+
+ }
+
+
  void display()
  {
 
@@ -21,7 +50,7 @@ public :
 
 int main()
 {
-    Student s1;
+    /*Student s1;
     //Creates memory  directly having garbage value.
     // parameters are age, rollNumber.
 
@@ -73,6 +102,38 @@ int main()
     Student *s10 = new Student;
 
     s10 -> display();
+
+    */
+
+    // our new constructor is called now
+    Student s12;
+    s12.display();
+    Student s34;
+    s34.display();
+
+
+    Student s9(12);
+    cout << "parameterized " << endl;
+
+    s9.display();
+
+    Student *s45 = new Student(101);
+    s45 -> display();
+
+    //Here we are passing two values for age and roll no both.
+    Student s67(34,56);
+    s67.display();
+
+    Student *s404 = new Student(23,45);
+    s404 -> display();
+
+
+
+
+
+
+
+
 
 
 
